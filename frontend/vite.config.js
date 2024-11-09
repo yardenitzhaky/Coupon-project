1e95b7ec3bd8c4c0121767ca0fa8bd406b11c49b
yarden_itzhaky <yardene015@gmail.com>
2024-11-09 14:08:50 +0200

    changed some dependencies, installed packages (tailwind) and added .env file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  }
 })
